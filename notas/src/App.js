5f6af570027ea78a3fd0157fdf1ef3d2a49dc0d0
pastafarista <antonio.cabrera.landin@gmail.com>
2023-11-16 12:14:18 +0100

    feature: diseño básico de la página

--- notas/src/App.js
+++ notas/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div class="container text-center">
+        <h1>Aplicación de Notas</h1>
+      </div>
+
+      <div class="container mt-3">
+        <div class="row row-cols-2 border">
+          <div class="col-2 border">
+            Buscador
+          </div>
+
+          <div class="col-2">
+            <input type="text" class="form-control" placeholder="Buscar"/>
+          </div>
+        </div>
+      </div>
+    
+      <div class="container mt-3 text-center">
+        <h1>Insertar Nota</h1>
+      </div>
+
+        <div class="container mt-3">
+          <div class="row row-cols-2 border">
+
+            <div class="col-2 border">
+              Titulo
+            </div>
+
+            <div class="col-2">
+              <input type="text" class="form-control" placeholder="Titulo"/>
+            </div>
+            
+          </div>
+
+          <div class="container mt-3">
+            <input type="text" class="form-control" placeholder="Contenido"/>
+          </div>
+
+          <button type="button" class="btn btn-primary mt-3">Guardar</button>
+
+      </div>
+
+
+
     </div>
   );
 }

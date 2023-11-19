c11fc4d1885be65d7adb2e20af0fc9f9f616b43c
pastafarista <antonio.cabrera.landin@gmail.com>
2023-11-19 19:51:35 +0100

    feature: Las notas se guardan en LocalStorage permitiendo mantener notas al recargar la página

--- notas/src/App.js
+++ notas/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NoteList from './componentes/NotesList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
-import AddNote from './componentes/AddNote';
 import Search from './componentes/Search';
 
 function App() {
-
   const [notes, setNotes] = useState([
     {
     id: nanoid(),
@@ -28,6 +26,21 @@ function App() {
 
   const [searchText, setSearchText] = useState('');
 
+  // Obtenemos las notas del localStorage
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem('notas')); // Obtenemos las notas del localStorage
+
+    if(savedNotes) // Si hay notas guardadas en el localStorage
+    {
+      setNotes(savedNotes);
+    }
+  }, []); // Se ejecuta solo una vez (cuando se carga la pagina por primera vez)
+
+  // Guardamos las notas en el localStorage
+  useEffect(() => {
+    localStorage.setItem('notas', JSON.stringify(notes)); // Guardamos las notas en el localStorage
+  }, [notes]);
+
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -44,7 +57,6 @@ function App() {
     setNotes(notes.filter((note) => note.id !== id)); // Seteamos las notas a un array sin la nota que eliminamos
   }
 
-
   return (
     <div className="container">
 
@@ -53,7 +65,6 @@ function App() {
       <NoteList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
     </div>
   );
-
 }
 
 export default App;
